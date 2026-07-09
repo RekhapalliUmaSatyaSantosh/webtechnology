79faa2aed6418112882e6881846179867d3ba55e
Satya Santosh <166365853+RekhapalliUmaSatyaSantosh@users.noreply.github.com>
2026-07-09 09:09:19 +0530

    Add operators.js with arithmetic, assignment, and logical examples

---
+++ javascript/day3/operators.js
@@ -0,0 +1,40 @@
+var a=10,b=20
+//arthimetic
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+console.log(++a);
+console.log(a++);
+console.log(a);
+console.log(--b);
+console.log(b--);
+console.log(b);
+
+//assigment
+var b=10
+console.log(a+=5);
+console.log(a-=5);
+console.log(a*=5);
+console.log(a/=5);
+console.log(a%=5);
+console.log(a**=5);
+
+// relational
+console.log(5=='5');
+console.log(5==5);
+console.log(5==='5');
+console.log(5===5);
+console.log(5!='5');
+console.log(5!=='5');
+console.log(5<4);
+console.log(5>4);
+console.log(5<=4);
+console.log(5>=4);
+
+//logical
+console.log((a>b)||(a<b));
+console.log((a>b)&&(a<b));
+console.log((a>b)!=(a<b));
