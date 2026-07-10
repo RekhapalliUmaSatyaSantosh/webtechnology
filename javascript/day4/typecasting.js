9ae84e5963197126f8d798096c225c4c38881c64
Satya Santosh <166365853+RekhapalliUmaSatyaSantosh@users.noreply.github.com>
2026-07-10 09:17:25 +0530

    Create typecasting.js with typecasting examples
    Add examples of implicit and explicit typecasting in JavaScript.

---
+++ javascript/day4/typecasting.js
@@ -0,0 +1,12 @@
+//implict typecasting
+let a=10,b='20'
+console.log(a+b);
+console.log(a*b);
+console.log(a-b);
+console.log(b%a);
+console.log(b/a);
+console.log(b);
+console.log(b++);
+console.log(++b);
+
+//explicit typecasting
