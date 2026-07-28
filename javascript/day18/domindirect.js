abaf270caf471fec02a65c4c72e622a2e89d7e7f
Satya Santosh <166365853+RekhapalliUmaSatyaSantosh@users.noreply.github.com>
2026-07-28 09:47:10 +0530

    Create domindirect.js for DOM selector examples
    Add DOM manipulation examples using various selectors.

---
+++ javascript/day18/domindirect.js
@@ -0,0 +1,29 @@
+let h2=document.getElementById('heading')
+console.log(h2);
+
+let cls=document.getElementsByClassName('para')
+console.log(cls);
+
+let h1=document.getElementsByTagName('h1')
+console.log(h1);
+
+let sec=document.getElementsByName('sec')
+console.log(sec);
+
+let as=document.querySelector('#aside')
+console.log(as);
+
+console.log(document.querySelector('h1'));
+
+let all=document.querySelectorAll('h1')
+console.log(all);
+
+console.log(document.querySelectorAll('div aside'));
+
+let header=document.querySelector('header')
+console.log(header);
+
+console.log(header.childNodes);
+
+let container=document.querySelectorAll('footer')
+console.log(container);
